d41e0e153a405eac37162a7150a8563826176d95
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-08-11 00:37:55 +0900

    Update category.js

--- app/javascript/packs/category.js
+++ app/javascript/packs/category.js
@@ -6,22 +6,12 @@ document.addEventListener('turbolinks:load', function () {
 
   const categoryFormsWrapper = document.querySelector('#category-forms'); // カテゴリのフォームたちの親要素
 
-  // 変更されたカテゴリのフォームより後ろにあるカテゴリのフォームを消すための関数
-  const removeSiblings = (startElement, className) => { // startElementの後ろの要素のうち、classNameと一致する要素を全て削除する
-    let nextElement = startElement.nextSibling;
-    while (nextElement) {
-      const checkElement = nextElement;
-      nextElement = nextElement.nextSibling;
-      if (checkElement.className == className) checkElement.remove();
-    }
-  }
-
   const categoryChanged = (e) => { // カテゴリが変更された時、ajax通信を行い次のフォームを組み立てて追加する
     const selectedCategoryId = e.target.value; // 選択されたカテゴリのid
     const changedForm = e.target; // 選択されたカテゴリのフォーム
 
     if (!selectedCategoryId) { // 選択されたカテゴリのidが-----だった場合、後ろのフォームを消して終了
-      removeSiblings(changedForm, "category_select_form");
+      changedForm.nextSibling.remove();
       return false
     }
 
@@ -44,13 +34,13 @@ document.addEventListener('turbolinks:load', function () {
 
       if (categories.length == 0) return false; // カテゴリたちが0個 = 孫カテゴリが選択されたので終了
 
-      removeSiblings(changedForm, "category_select_form"); // 変更されたカテゴリのフォームより後ろにあるカテゴリのフォームを消す
+      if (changedForm.nextSibling) changedForm.nextSibling.remove(); // 変更されたカテゴリのフォームより後ろにあるカテゴリのフォームを消す
 
       const newElement = document.createElement("div"); // addEventListnerのためにElementオブジェクトを作成
       newElement.innerHTML = buildSelectForm(categories); // Elementオブジェクトに新しいカテゴリのフォームを挿入
       const newSelectFormElement = newElement.childNodes[0]; // newElementの最初の子要素が新しいカテゴリのフォーム
       newSelectFormElement.addEventListener("change", categoryChanged); // 新しいカテゴリのフォームにイベントを追加
-      categoryFormsWrapper.insertAdjacentElement("beforeend", newSelectFormElement); // 新しいカテゴリのフォームをビューに表示する
+      changedForm.insertAdjacentElement("afterend", newSelectFormElement); // 新しいカテゴリのフォームをビューに表示する
     }
 
   } // categoryChanged()ここまで
@@ -60,10 +50,12 @@ document.addEventListener('turbolinks:load', function () {
     categories.forEach(category => {
       options += buildOption(category);
     });
-    let html = `<select class="category_select_form" name="category_id">
-                  <option value="">-----</option>
-                  ${options}
-                </select>`;
+    let html = `<div>
+                  <select class="category_select_form" name="category_id">
+                    <option value="">-----</option>
+                    ${options}
+                  </select>
+                </div>`;
     return html;
   }
 
